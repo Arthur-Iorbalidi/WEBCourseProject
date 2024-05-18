eacfc1f745096823dcec02b1f7563e85b803769e
Arthur Iorbalidi <arturiorbalidi01052005@gmail.com>
2024-05-18 19:30:29 +0300

    feat: add reset mistake messages, add reload page after localstorage reset

--- CourseProject/js/MainPage/App/components/Authorization/Authorization.js
+++ CourseProject/js/MainPage/App/components/Authorization/Authorization.js
@@ -90,6 +90,9 @@ class Authorization {
     clearInputs() {
         document.querySelector('.inputEmailAuthorization').value = '';
         document.querySelector('.inputPasswordAuthorization').value = '';
+        document.querySelector('.mistakeMessageEmailAuthorization').textContent = '';
+        document.querySelector('.mistakeMessagePasswordAuthorization').textContent = '';
+        document.querySelector('.submitAuthorization').disabled = true;
     }
 
     submit(event) {
@@ -126,6 +129,9 @@ class Authorization {
                 else {
                     document.querySelector('.mistakeMessagePasswordAuthorization').textContent = 'Incorrect password';
                 }
+            }
+            else {
+                document.querySelector('.mistakeMessageEmailAuthorization').textContent = 'There isn\'t such user';
             }
         }
     }
